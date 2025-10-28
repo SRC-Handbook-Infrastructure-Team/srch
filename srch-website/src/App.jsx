be836e4b56c1321ce088c9115bf0eee532066a91
“Will” <william_tolmie@brown.edu>
2025-10-28 18:04:23 -0400

    more search bugs

--- srch-website/src/App.jsx
+++ srch-website/src/App.jsx
@@ -2,7 +2,6 @@
 import "./App.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import theme from "./theme";
 import AppRoutes from "./AppRoutes"
 
